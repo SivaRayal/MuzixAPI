670cacc774dc5160ac64a55d427c6874817bc052
Sivasai.Kuruva@ness.com <Sivasai.Kuruva@ness.com>
2023-11-09 14:48:14 +0000

    postman and db scripts update

---
+++ SCRIPTS/db/user_db.sql
@@ -0,0 +1,3 @@
+use fsd;
+select * from user_profile;
+insert into user_profile values("kuruva.siva8055@gmail.com",8899223344,"Siva Sai Krishna","Kuruva","$2a$04$6Sd7OycRfKSiPSlV/OOjBuX.yEofwvCb2oALtsOcG1KDXs7N8JoVu");
